a412168f13bffda53ae0bb08b362838cc7ee90b4
Saraj Dhakal <sarajdhakal>
2024-06-29 20:30:50 +0545

    Updating files

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 # Register your models here.
-# from projects.models import Project
 from .models.projects import Project
+from .models.employee import Employee
 
 admin.site.register(Project)
+admin.site.register(Employee)
---
+++ projects/models/employee.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE , null = True)
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
