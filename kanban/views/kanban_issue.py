5e1876c162d35e5995e252dc2efcea1a45cf6554
Saraj Dhakal <sarajdhakal>
2024-09-10 13:37:00 +0545

    Laetest commit...

--- kanban/views/kanban_issue.py
+++ kanban/views/kanban_issue.py
@@ -1,22 +1,51 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
 from projects.models import Project
+from projects.models.employee import Employee
 from ..models.kanban import Issue
+from django.core.files.storage import default_storage
 
-
-# @login_required(login_url='login')
 def kanban_issue(request, id):
     try:
-        issue = Issue.objects.get(id=id)
+        issue = get_object_or_404(Issue, id=id)
         projects = Project.objects.all()
         users = User.objects.all()
+        employees = Employee.objects.all()
+
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            status = request.POST.get('status')
+            labels = request.POST.get('labels')
+            project_name = request.POST.get('project').strip()
+            desc = request.POST.get('issue_2description')
+            attachment = request.FILES.get('attachment', None)
+
+            project = get_object_or_404(Project, name__iexact=project_name)
+
+            issue.title = title
+            issue.description = description
+            issue.status = status
+            issue.labels = labels
+            issue.project = project
+            issue.issue_2description = desc
+
+            if attachment:
+
+                if issue.attachment:
+                    default_storage.delete(issue.attachment.path)
+                issue.attachment = attachment
+            issue.save()
+            return redirect('kanban')
+
         context = {
             'issue': issue,
             'projects': projects,
             'users': users,
+            'employees': employees
         }
         return render(request, 'kanban_issue.html', context)
+
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
+        return redirect('kanban')
