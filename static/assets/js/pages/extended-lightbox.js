9e455699bf7e56ba5c1e49de8b660d76ed99d00a
Saraj Dhakal <sarajdhakal>
2024-09-12 13:52:39 +0545

    Latest Commit

--- static/assets/js/pages/extended-lightbox.js
+++
@@ -1,1 +0,0 @@
-var lightbox=GLightbox({selector:".image-popup",title:!1}),lightboxDesc=GLightbox({selector:".image-popup-desc"}),lightboxvideo=GLightbox({selector:".image-popup-video-button",title:!1}),lightboxvideo=GLightbox({selector:".image-popup-video",title:!1}),lightboxvideo=GLightbox({selector:".image-iframe-elements",title:!1});
--- static/assets/js/pages/extended-plyr.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){window.Plyr&&new Plyr("#player-youtube")}),document.addEventListener("DOMContentLoaded",function(){window.Plyr&&new Plyr("#player-charlotte")});
